dc00be8520c7cc251bdf411d97d94969c010deb2
wGuerrero333 <wguerreroc1969@gmail.com>
2024-01-10 15:30:18 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// la forma por defualt es type= jscommon la otra es la forma modular
+// const express = required('express')
+import  express  from "express"
+// import ejs from "ejs" se importa pero como viene intergado a node no es necesario
+// estas son utiles para encontrar la direccion absoluta de la carpeta views
+import {dirname, join} from 'path';
+import { fileURLToPath} from 'url'
+
+import indexRouters from './routes/index.js'
+
+const app = express()
+// forma dinamica de encontar la ruta ABSOLUTA
+const __dirname =  dirname(fileURLToPath(import.meta.url))
+// console.log(__dirname) 
+// join() concatena las rutas sin importar que sean windows o linux es equivalente a
+app.set('views',join(__dirname,'vistas'))
+// establece el view engine para incorporar ejs  ~~ ( html enriquecido osea .ejs)
+app.set('view engine', 'ejs')
+
+
+
+// cuando envie una peticion GET importa indexRoutes desde routes y haga la peticion
+app.use(indexRouters)
+
+// para ue express le permita usar la carpeta public ~~ static
+app.use(express.static(join (__dirname, 'public') ))
+// en el navegar se puede acceder al main.css asi: http://localhost:3000/main.css
+
+const puerto = 3000
+app.listen(puerto)
+console.log("Escucha por" , puerto)
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import { Router } from "express"
+// estas variables inyectadas se usan en el .ejs <%= variable %>
+const router = Router()
+// pasar variables es de gran utlidad para pasar datos de la db
+router.get("/", (req, res) => res.render("index", {titulo : "Titulo desde backends" ,variable : "Desdebackend"}))
+router.get("/productos", (req, res) => res.render("productos"))
+router.get("/about", (req, res) => res.render("about",{titulo : "Titulo desde backends" ,variable : "Abby Hot desde Backend"}))
+
+export default router
